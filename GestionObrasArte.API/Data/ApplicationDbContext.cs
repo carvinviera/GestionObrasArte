0c47f570c53da89aaa5f77eca6262e5beb1a1e8e
carvinviera <carvinviera@gmail.com>
2025-10-27 11:41:41 +0100

    Configurar DbContext, appsettings, Program.cs y modificar .gitignore para incluir la BD

---
+++ GestionObrasArte.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using GestionObrasArte.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionObrasArte.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Artista> Artistas { get; set; }
+        public DbSet<TipoPintura> TiposPintura { get; set; }
+        public DbSet<Pintura> Pinturas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Artista>(entity =>
+            {
+                entity.HasKey(e => e.IdArtista);
+                entity.Property(e => e.NombreArtista).IsRequired();
+                entity.Property(e => e.ApellidosArtista).IsRequired();
+                entity.Property(e => e.AñoNacimientoArtista).IsRequired();
+            });
+
+            modelBuilder.Entity<TipoPintura>(entity =>
+            {
+                entity.HasKey(e => e.IdTipoPintura);
+                entity.Property(e => e.TítuloTipoPintura).IsRequired();
+            });
+
+            modelBuilder.Entity<Pintura>(entity =>
+            {
+                entity.HasKey(e => e.IdPintura);
+                entity.Property(e => e.TituloPintura).IsRequired();
+                entity.Property(e => e.Precio).HasColumnType("DECIMAL");
+
+                entity.HasOne(d => d.Artista)
+                      .WithMany()
+                      .HasForeignKey(d => d.Fk_IdArtista);
+
+                entity.HasOne(d => d.TipoPintura)
+                      .WithMany()
+                      .HasForeignKey(d => d.FK_IdTipoPintura);
+            });
+        }
+    }
+}
--- GestionObrasArte.API/Program.cs
+++ GestionObrasArte.API/Program.cs
@@ -1,41 +1,51 @@
+using GestionObrasArte.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// 1. Añadir servicios al contenedor.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 2. Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 3. Configurar el pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// 4. Usar CORS
+app.UseCors("AllowAll");
 
-app.MapGet("/weatherforecast", () =>
+app.UseAuthorization();
+app.MapControllers();
+
+// 5. Aplicar migraciones al arrancar
+using (var scope = app.Services.CreateScope())
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
